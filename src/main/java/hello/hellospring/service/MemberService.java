a313e709c89705e6ae5d4d66eb5ae0a70c1e9691
huiji <fmti0720@gmail.com>
2022-01-16 22:09:33 +0900

    spring(success)

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 
 @Transactional
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -24,11 +25,17 @@ public class MemberService {
 
 
     //회원가입
-    public Long join(Member member){
-
-        validateDuplicateMember(member); //중복회원 검증
-        memberRepository.save(member); //멤버 저장
-        return member.getId();
+    public Long join(Member member) {
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     //같은 이름이 있는 중복회원X
@@ -40,12 +47,14 @@ public class MemberService {
     }
 
     //전체 회원 조회
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
+    public List<Member> findMembers() {
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
-
-    public Optional<Member> findOne(Long memberId){
-        return memberRepository.findById(memberId);
-    }
-
 }
